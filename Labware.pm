e1662e57691565af1c4ab1d3cdac670de65ec3e2
Richard White <richard.white@sanger.ac.uk>
2013-08-15 11:40:33 +0100

    New file Labware.pm
    Labware.pm - Base Module

---
+++ Labware.pm
@@ -0,0 +1,19 @@
+## no critic (RequireUseStrict, RequireUseWarnings, RequireTidyCode)
+package Labware;
+## use critic
+
+# ABSTRACT: Modules to represent Labware (Plates and Wells)
+
+## Author         : rw4
+## Maintainer     : rw4
+## Created        : 2013-08-15
+## Last commit by : $Author$
+## Last modified  : $Date$
+## Revision       : $Revision$
+## Repository URL : $HeadURL$
+
+use warnings;
+use strict;
+use autodie;
+
+1;
